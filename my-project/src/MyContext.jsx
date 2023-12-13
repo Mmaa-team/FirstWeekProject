f65948a8dd244c5132f5f5dc0e2dc2282f203264
manar <manar.bouaouina@gmail.com>
2023-12-13 16:11:00 +0100

    About

--- my-project/src/MyContext.jsx
+++ my-project/src/MyContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useEffect, useState } from "react";
 
 export const MyContext = createContext()
 export const ContextProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user") || {}))
+    const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user") || null))
     const login = async (input) => {
         try {
             const res = await axios.post('http://127.0.0.1:5000/api/auth/signin', input)
