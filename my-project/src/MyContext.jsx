2bef85b420bd458fa2f7bb98ae836bc330a3451d
Maryem-Harmessi <hermassimyriam@gmail.com>
2023-12-13 16:04:46 +0100

    basket back untested

--- my-project/src/MyContext.jsx
+++ my-project/src/MyContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useEffect, useState } from "react";
 
 export const MyContext = createContext()
 export const ContextProvider = ({ children }) => {
-    const [currentUser,setCurrentUser]=useState(JSON.parse(localStorage.getItem("user")|| {}))
+    const [currentUser,setCurrentUser]=useState(JSON.parse(localStorage.getItem("user")|| null))
     const login =async (input)=>{
         try{
         const res= await axios.post('http://127.0.0.1:5000/api/auth/signin',input)
@@ -14,7 +14,7 @@ export const ContextProvider = ({ children }) => {
      }
      const logout=async(input)=>{
         await axios.post('http://127.0.0.1:5000/api/auth/logout')
-       setCurrentUser({})
+       setCurrentUser(null)
    }
    useEffect(()=>{
     localStorage.setItem("user",JSON.stringify(currentUser))
