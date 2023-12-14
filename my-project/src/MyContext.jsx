2e88664ae05bb6fd4e6496ad1b29a282c38d5b3a
Ahmed Haddada <haddadaahmed9@gmail.com>
2023-12-14 10:05:51 +0100

    header

--- my-project/src/MyContext.jsx
+++ my-project/src/MyContext.jsx
@@ -1,27 +1,32 @@
-import React, { createContext } from "react";
-
+import React, { createContext, useState, useEffect } from 'react'
 
 export const MyContext = createContext()
 export const ContextProvider = ({ children }) => {
-    const [currentUser,setCurrentUser]=useState(JSON.parse(localStorage.getItem("user")|| {}))
-    const login =async (input)=>{
-        try{
-        const res= await axios.post('http://127.0.0.1:5000/api/auth/signin',input)
-        setCurrentUser(res.data)}
-        catch(err){
+    const [currentUser, setCurrentUser] = useState(
+        JSON.parse(localStorage.getItem('user') || null)
+    )
+
+    const login = async (input) => {
+        try {
+            const res = await axios.post(
+                'http://127.0.0.1:5000/api/auth/signin',
+                input
+            )
+            setCurrentUser(res.data)
+        } catch (err) {
             console.log(err)
         }
-     }
-     const logout=async(input)=>{
+    }
+    const logout = async (input) => {
         await axios.post('http://127.0.0.1:5000/api/auth/logout')
-       setCurrentUser({})
-   }
-   useEffect(()=>{
-    localStorage.setItem("user",JSON.stringify(currentUser))
-    },[currentUser])
-    return (
+        setCurrentUser({})
+    }
 
-        <MyContext.Provider value={{login,logout,currentUser}}>
+    useEffect(() => {
+        localStorage.setItem('user', JSON.stringify(currentUser))
+    }, [currentUser])
+    return (
+        <MyContext.Provider value={{ login, logout, currentUser }}>
             {children}
         </MyContext.Provider>
     )
