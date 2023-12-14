80f3a3b790125db4b51461c9c557b5d242af3144
Maryem-Harmessi <hermassimyriam@gmail.com>
2023-12-14 11:56:36 +0100

    some changes/lunch break commit

--- my-project/src/components/pages/Profile.jsx
+++ my-project/src/components/pages/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () => {
     return (
         <>
             <div className="h-screen min-h-full w-full  ">
-                <div className=" flex w-full flex-col items-center relative justify-center p-10">
+                <div className=" relative flex w-full flex-col items-center justify-center p-10">
                     <div className="relative">
                         <img
                             className="bg-container  h-[355px] w-[1040px] rounded-[5px] bg-black bg-opacity-50 brightness-50"
@@ -26,6 +26,29 @@ const Profile = () => {
                         <div className=" absolute bottom-0 right-0 flex h-10  w-10 cursor-pointer items-center  justify-center rounded-[150px] bg-indigo-500">
                             <FaCamera className="text-md    bg-indigo-500 text-white " />
                         </div>
+                    </div>
+                    
+                    <div className=" font-['SF Pro Display']  absolute bottom-[67px] text-[28px] font-semibold text-black">
+                        Farhan Khan
+                    </div>
+                    <div className="font-['SF Pro Display'] w-fit absolute md:bottom-[-4px] md:w-[896px] text-center text-base font-normal leading-[25px] text-black text-opacity-80">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Tortor, consectetur purus amet, accumsan nulla. Ut urna
+                        placerat morbi cursus pulvinar nunc adipiscing. Tortor,
+                        consectetur purus amet, accumsan nulla. Ut urna placerat
+                        morbi cursus pulvinar nunc adipiscing.
+                    </div>
+                    <div className=" absolute inline-flex h-[45px] w-[164px] right-[230px] bottom-[90px] items-center justify-center gap-2.5 rounded-[150px] bg-yellow-900 p-2.5">
+                        <div className="absolute font-['SF Pro Display'] text-base font-semibold text-white">
+                            Edit Profile
+                        </div>
+                    </div>
+                </div>
+                <div  className="w-[345px] h-[371px] bg-white bg-opacity-10 rounded-[5px]" > 
+                </div>
+                <div>
+                    <div>
+                        <img className="w-[666px] h-[426px] rounded-[5px]" src="https://via.placeholder.com/666x426" />  
                     </div>
                 </div>
             </div>
