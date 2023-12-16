dfceb7920530165596f465a02260be019032c2d3
aziza0412 <azizabenhalima@live.fr>
2023-12-16 14:56:48 +0100

    new context

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ContextProvider } from "./MyContext.jsx";
+import { UserContextProvider } from './Usercontext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ContextProvider>
+    < UserContextProvider >
+
       <App />
+      </ UserContextProvider> 
     </ContextProvider>
   </React.StrictMode>,
 )
